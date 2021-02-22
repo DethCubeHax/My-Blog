2ccb9694814f59acbd07d8b2a569669dcaf3c7f3
RownakCodes <rownak.shr@gmail.com>
2021-02-22 20:56:39 +0600

    implemented reading list for reading later with sum problem-solving-aura halp from my buddy :)

--- app.py
+++ app.py
@@ -103,15 +103,20 @@ def passwordUpdate():
         db.execute("UPDATE users SET password=:password WHERE email=:email", password=generate_password_hash(newPassword), email=email)
         return render_template("account.html", msg = "Credentials changed successfully!", data=tmp[0])
 
-@app.route("/readingList", methods=['POST'])
+@app.route("/readingList", methods=['POST', 'GET'])
 @login_required
 def readingList():
-    print('Request method: ' + request.method)
     if request.method == "POST":
+        if request.form.get("del"):
+            contentID = request.form.get("contentID")
+            db.execute("DELETE FROM readLater WHERE userID=:userID AND contentID=:contentID", userID=session["user_id"], contentID=contentID)
+            return redirect("/readingList")
         contentID = request.form.get("id")
-        print('ID: ' + contentID)
-    else:
-        tmp = db.execute("SELECT * FROM readingList WHERE id=:id", id=session["user_id"])
+        db.execute("INSERT INTO readLater (userID, contentID) VALUES (:userID, :contentID)", userID=session["user_id"], contentID=contentID)
+        return redirect("/")
+    if request.method == "GET":
+        data = db.execute("SELECT * FROM content JOIN readLater ON readLater.contentID=content.id WHERE readLater.userID=:userID", userID = session["user_id"])
+        return render_template("readingList.html", data = data)
 
 @app.route("/about")
 def about():
@@ -142,6 +147,14 @@ def deleteComment():
     commentData = db.execute("SELECT * FROM comments")
     path = rows[0]["path"]
     return render_template(path[7:] + "content.html", data=rows[0], commentData=commentData)
+
+@app.route("/history", methods=["GET", "POST"])
+@login_required
+def history():
+    if request.method == "GET":
+        data = db.execute("SELECT * FROM comments JOIN content ON comments.contentID=content.id WHERE userID=:userID", userID=session["user_id"])
+        contentData = []
+        return render_template("history.html", data=data, contentData=contentData)
 
 
 def update():
